54ef41fe8951ea23e6c6bc0060bbd4d72100e0bb
chineQin <1039733244@qq.com>
2022-03-14 00:41:11 +0800

    日常学习

---
+++ test_2022_3_14/test_2022_3_14/test.c
@@ -0,0 +1,28 @@
+#define CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+#include<string.h>
+#include<windows.h>
+
+int main()
+{
+	char arr1[] = "welcome to chine";
+	char arr2[] = "****************";
+	int left = 0;
+	int right = strlen(arr1) - 1;
+
+	while (left <= right)
+	{
+		arr2[left] = arr1[left];
+		arr2[right] = arr1[right];
+		printf("%s\n", arr2);
+		Sleep(500);//Ë¯Ãß0.1Ãë
+		system("cls");//Çå¿ÕÆÁÄ»
+		left++;
+		right--;
+
+	}
+	printf("%s\n", arr2);
+
+	return 0;
+}
